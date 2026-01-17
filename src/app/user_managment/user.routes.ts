73f3a1fc7e94b80621c40c8675f67f1e647fcdbc
MIEHE Alix <alixmiehe2004@gmail.com>
2026-01-17 18:34:18 +0100

    remove some useless dependency

--- src/app/user_managment/user.routes.ts
+++ src/app/user_managment/user.routes.ts
@@ -3,7 +3,6 @@ import { FormCreateUser } from './components/form-create-user/form-create-user';
 import { FormUserLogin } from './components/form-user-login/form-user-login';
 import { ListUsers } from './components/list-users/list-users';
 import { IsNotLoggedInGuard } from '../auth_managment/guards/is-not-logged-in-guard';
-import { IsLoggedInGuard } from '../auth_managment/guards/is-logged-in-guard';
 import { IsAdminGuard } from '../auth_managment/guards/is-admin-guard';
 import { CreateUserAccessGuard } from './guards/create-user-access-guard';
 
